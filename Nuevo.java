f106c25b0eb863538b66223abec9ea19efd96c20
alfonsocq <alfcastroq@gmail.com>
2022-03-28 13:17:42 -0500

    inka

---
+++ Nuevo.java
@@ -0,0 +1,12 @@
+public class Nuevo {
+    public static void main(String[] args) {
+
+        Saludos hiApp = new Saludos();
+        System.out.println(hiApp.hola("Will"));
+        System.out.println(hiApp.edad("53"));
+        System.out.println(hiApp.lugar("Los Angeles"));
+        
+
+
+    }
+}
---
+++ saludos.java
@@ -0,0 +1,16 @@
+class Saludos {
+
+
+    public static String hola(String nombre) {
+        return "Mi nombre es "+nombre+".";
+    }
+
+    public static String edad(String crono) {
+        return "Tengo "+crono+" años.";
+    }
+
+    public static String lugar(String ciudad) {
+        return "Mi ciudad natal es "+ciudad+".";
+    }
+
+}
